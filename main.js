fd556fc7a6da1f2cdeaeee34ef974a100acb53f4
unknown <xydozhnikbomj@gmail.com>
2023-05-23 18:46:04 +0300

    1.1

--- main.js
+++ main.js
@@ -10,36 +10,10 @@ rainbow.splice(1, 3);
 rainbow.push('Gave','Battle', destination[1], 'Vain');
 console.log(rainbow);
 
-for (i = 0; i < rainbow.length; i++) {
-  element = rainbow[i];
-  colorClass = '';
-
-  switch (element) {
-    case 'Richard':
-      colorClass = 'red';
-      break;
-    case 'Of':
-      colorClass = 'orange';
-      break;
-    case 'York':
-      colorClass = 'yellow';
-      break;
-    case 'Gave':
-      colorClass = 'green';
-      break;
-    case 'Battle':
-      colorClass = 'blue';
-      break;
-    case 'In':
-      colorClass = 'indigo';
-      break;
-    case 'Vain':
-      colorClass = 'violet';
-      break;
-    default: 
-    colorClass = 'red';
-    break;
-  }
-  document.write(' <div class="circle ' + colorClass + '"> </div>');
-  document.write('<div class="text">' + element + '</div>');
+colorClasses = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+for ( i = 0; i < rainbow.length; i++ ) {
+ element = rainbow[i];
+ colorClass = colorClasses[i] || 'red';
+ document.write('<div class="circle ' + colorClass + '"></div>');
+ document.write('<div class="text">' + element + '</div>');
 }
